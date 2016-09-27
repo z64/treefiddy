cb204dab06a3918dc1f966438f48a87c7e363c5c
z64 <zachnowicki@gmail.com>
2016-09-27 17:45:35 +0000

    register events

--- src/treefiddy.cr
+++ src/treefiddy.cr
@@ -8,6 +8,10 @@ module TreeFiddy
   # Create the bot
   BOT = Bot.new(CONFIG.token, CONFIG.client_id)
 
+  # Register events
+  Event::Ping.register(BOT.client)
+  Event::Add.register(BOT.client)
+
   # Run the bot
   BOT.run!
 end
