f27aaf3dcca9b1dd9b6096fec917415c33aae0b6
z64 <zachnowicki@gmail.com>
2016-09-27 14:54:36 +0000

    add main program

--- src/treefiddy.cr
+++ src/treefiddy.cr
@@ -1,5 +1,13 @@
+require "discordcr"
 require "./treefiddy/*"
 
 module TreeFiddy
-  # TODO Put your code here
+  # Bot configuration
+  CONFIG = Config.from_yaml(File.read("data/config.yml"))
+
+  # Create the bot
+  BOT = Bot.new(CONFIG.token, CONFIG.client_id)
+
+  # Run the bot
+  BOT.run!
 end
